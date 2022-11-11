d9065391df620cdb848173bde6d2ebc18416f794
Surfer0997 <artanagibaet228@gmail.com>
2022-11-11 09:15:16 +0200

    Router fix

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,7 @@ import { Footer } from './components/Footer/Footer';
 import { Header } from './components/Header/Header';
 import { AboutUs } from './components/Main/AboutUs/AboutUs';
 import { ToPartners } from './components/Main/To partners/ToPartners';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route, Outlet } from 'react-router-dom';
 import { Shop } from './components/Main/Shop/Shop';
 import { Cart } from './components/Cart/Cart';
 import { ThemeSwitch } from './components/UI/ThemeSwitch';
@@ -13,17 +13,19 @@ function App() {
   return (
     <>
       <Background />
-      <ThemeSwitch/>
+      <ThemeSwitch />
       <Router>
         <div className="App">
           <Header />
-          <Cart/>
+          <Cart />
           <main>
             <Routes>
-              <Route path="/about" element={<AboutUs />} />
-              <Route path="/shop" element={<Shop />} />
-              <Route path="/partners" element={<ToPartners />} />
-              <Route path="*" element={<Shop />} />
+              <Route path="/ultra-high-tech/">
+                <Route path="/ultra-high-tech/about" element={<AboutUs />} />
+                <Route path="/ultra-high-tech/shop" element={<Shop />} />
+                <Route path="/ultra-high-tech/partners" element={<ToPartners />} />
+                <Route path="/ultra-high-tech/*" element={<Shop />} />
+              </Route>
             </Routes>
           </main>
           <Footer />
---
+++ src/helpers/useDebounce.ts
@@ -0,0 +1,19 @@
+import { useEffect } from "react";
+
+export const useDebounce = (callback:any, delay:number) => {
+    useEffect(
+        () => {
+          // Update debounced value after delay
+          const handler = setTimeout(() => {
+            callback();
+          }, delay);
+          // Cancel the timeout if value changes (also on delay change or unmount)
+          // This is how we prevent debounced value from updating if value is changed ...
+          // .. within the delay period. Timeout gets cleared and restarted.
+          return () => {
+            clearTimeout(handler);
+          };
+        },
+        [callback, delay] // Only re-call effect if value or delay changes
+      );
+}
